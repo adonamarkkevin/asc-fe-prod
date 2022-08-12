4b4bab92f6a3c51f9ee9c9bf26ab9e2b73b4d502
unknown <adonamarkkevin@gmail.com>
2022-08-12 21:55:55 +0800

    changed deply.h

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ docker stop asc_prod_fe
 docker rm asc_prod_fe
 docker rmi asc-fe
 
-docker build -t asc-fe . --memory 2048MB
+docker build -t asc-fe .
 docker run -d -p 4200:4200 --name asc_prod_fe asc-fe
 docker image prune -f
